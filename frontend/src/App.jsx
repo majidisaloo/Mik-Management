055beda42e7cf4c66ca732b5981957b2052a50d9
majidisaloo <34711314+majidisaloo@users.noreply.github.com>
2025-10-13 16:51:00 +0300

    Add login endpoint and deployment update guidance

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
